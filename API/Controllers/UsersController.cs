26670e320be2949b42abb2fa6431774fa02c17ad
petruda <petru650@gmail.com>
2023-11-27 17:19:08 +0200

    fixed the routing

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,8 +3,11 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
-[ApiController]
+
+
 [Route("api/[controller]")] // create route for the api EX : //HTTPS:LOCALHOST:5001/API/USERS not with caps lock
+[ApiController]
+
 public class UsersController : ControllerBase
 {
     private readonly DataContext _context;
